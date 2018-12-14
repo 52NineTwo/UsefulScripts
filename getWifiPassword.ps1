ddb3af2dff69f81c9a5c1c9f842798fffae24cd9
root <root@localhost.localdomain>
2018-12-14 17:48:04 +0800

    Add get wifi password powershell script

---
+++ getWifiPassword.ps1
@@ -0,0 +1,9 @@
+
+function Get-WifiPassword(){
+    foreach ($SSID in (netsh wlan show profiles | Select-String ": (.*)"|% { $_.Matches.Groups[1].Value })){
+        [string]$password = netsh wlan show profiles name=$SSID key=clear | Select-String "¹Ø¼üÄÚÈÝ.*:(.*)"|% { $_.Matches.Groups[1].Value }
+        "{0,-20} : {1,10}" -f $SSID, $password
+    }
+}
+
+Get-WifiPassword
